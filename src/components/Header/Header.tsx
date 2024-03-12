02bc1531fe0346294ec7c0aeedc00b71b09bf120
Dhruv Rastogi <d.rastogi15@gmail.com>
2024-03-12 12:04:00 +0530

    Full Page Poster

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,10 @@ import AppPoster from "../Assets/AppPoster.png"
 export default function Header() {
     return (
         <div>
-            <div style={{ flexDirection: 'row',flex:1 }}>
-                <h1 style={{fontFamily:'cursive'}}>Furs N Pets</h1>
-                <img style={{  }} src={AppPoster} loading="lazy" alt="logo"></img>
-            </div>
+            {/* <div style={{ flexDirection: 'row',flex:1 }}> */}
+                {/* <h1 style={{fontFamily:'cursive'}}>Furs N Pets</h1> */}
+                <img style={{ width:'100%' }} src={AppPoster} loading="lazy" alt="logo"></img>
+            {/* </div> */}
             {/* <nav>
         <Link to="/">Home</Link> |{" "}
         <Link to="about">About</Link>
